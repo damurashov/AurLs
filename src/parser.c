93564af38d3f7a1e3c3e2a72ad0414502d4ccb41
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-17 16:49:20 +0300

    INTERMEDIATE

---
+++ src/parser.c
@@ -0,0 +1,11 @@
+#include "parser.h"
+#include "dmls.h"
+#include <assert.h>
+
+void dmls_arguments_init_with_sys(DmlsArguments *arguments, const char **textargs, int nargs)
+{
+	assert(arguments != null);
+
+	if (nargs == 3) {
+	}
+}
