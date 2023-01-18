e30b0c83452131ebad099a19ec50bf427ac280f7
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-18 12:56:52 +0300

    [src:main] Bug | Flags checking

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main(int argc, const char **argv)
 	DmlsArguments dmls_arguments;
 
 	if (dmls_arguments_init_with_sys(&dmls_arguments, argv, argc) == DmlsResultOk) {
-		if (dmls_arguments.flags & DmlsFlagsList) {
+		if (dmls_arguments.flags & (DmlsFlagsList | DmlsFlagsAll)) {
 			if (dmls_iter_directory(dmls_arguments.directory,
 					printf_dmls_dir_entry) != DmlsResultOk) {
 				printf("Error: %s", dmls_last_error_message());
