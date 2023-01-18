972edd634841225b9bda9ba9c25add5f120fe462
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-18 09:24:58 +0300

    [src:parser] Ref | Magic numbers, DmlsResult

--- src/parser.c
+++ src/parser.c
@@ -4,20 +4,27 @@
 #include <stddef.h>
 #include <string.h>
 
+static const char *s_error_wrong_arguments = "Wrong arguments. Usage `dmls -l DIRECTORY";
+
 int dmls_arguments_init_with_sys(DmlsArguments *arguments, const char **args, int nargs)
 {
 	assert(arguments != NULL);
-	int result = 0;
+	int result = DmlsResultOk;
 
 	if (nargs == 3) {
 		if (strcmp(args[1], "-l") == 0) {
 			arguments->flags = DmlsFlagsList;
 		} else {
-			dmls_set_error_message("Wrong arguments. Usage `dmls -l DIRECTORY");
-			result = 1;
+			result = DmlsResultError;
 		}
 
 		arguments->directory = args[2];
+	} else {
+		result = DmlsResultError;
+	}
+
+	if (result != 0) {
+		dmls_set_error_message(s_error_wrong_arguments);
 	}
 
 	return result;
