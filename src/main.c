ab1b18a5b284975be10a84dba7a15e25a305c8d2
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-18 11:44:44 +0300

    [src:main] Impl | Initial integration

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
+#include "fs.h"
+#include "parser.h"
+#include "dmls.h"
 #include <stdio.h>
 
-int main(void)
+int main(int argc, const char **argv)
 {
-	printf("Hello, world\n");
+	DmlsArguments dmls_arguments;
+	if (dmls_arguments_init_with_sys(&dmls_arguments, argv, argc) == DmlsResultOk) {
+	} else {
+		printf("Error: %s", dmls_last_error_message());
+	}
 }
