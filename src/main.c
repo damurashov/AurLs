28e63fe7ce8437d81f4bab25de69b8fc491d4e13
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-18 11:55:05 +0300

    [src:main] Impl | Complete integration

--- src/main.c
+++ src/main.c
@@ -6,7 +6,14 @@
 int main(int argc, const char **argv)
 {
 	DmlsArguments dmls_arguments;
+
 	if (dmls_arguments_init_with_sys(&dmls_arguments, argv, argc) == DmlsResultOk) {
+		if (dmls_arguments.flags & DmlsFlagsList) {
+			if (dmls_iter_directory(dmls_arguments.directory,
+					printf_dmls_dir_entry) != DmlsResultOk) {
+				printf("Error: %s", dmls_last_error_message());
+			}
+		}
 	} else {
 		printf("Error: %s", dmls_last_error_message());
 	}
